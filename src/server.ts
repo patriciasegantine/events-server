fa7a7bda339d7750963bb17a0349cec2b6a9ac30
Patricia Segantine <pnsegantine@gmail.com>
2024-04-05 13:17:20 +0100

    feat: add zod, post route and linking server database

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,38 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify()
 
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
 app.get('/', () => {
   return 'Hello!'
 })
+
+app.post('/events',
+  async (request, reply) => {
+    
+    const createEventSchema = z.object({
+      title: z.string().min(3).max(50),
+      details: z.string().min(10).max(100).nullable(),
+      maximumParticipant: z.number().int().positive().nullable()
+    })
+    
+    const data = createEventSchema.parse(request.body)
+    
+    const event = await prisma.event.create({
+      data: {
+        title: data.title,
+        details: data.details,
+        maximumParticipant: data.maximumParticipant,
+        slug: 'ososidosid'
+      }
+    })
+    
+    return reply.status(201).send({eventId: event.id})
+  })
 
 app.listen({port: 3333}).then(() => console.log('HTTP server running at port 3333!'))
