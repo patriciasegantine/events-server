a1ccdf1d0ab50beb03130143864632924bcab386
Patricia Segantine <pnsegantine@gmail.com>
2024-04-05 11:13:29 +0100

    feat: setup project

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+
+app.get('/', () => {
+  return 'Hello!'
+})
+
+app.listen({port: 3333}).then(() => console.log('HTTP server running at port 3333!'))
