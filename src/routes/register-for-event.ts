6b625a236822b3372a1ec76c84482d0c991f2ee6
Patricia Segantine <pnsegantine@gmail.com>
2024-04-06 18:26:51 +0100

    feat: add register participant at event

---
+++ src/routes/register-for-event.ts
@@ -0,0 +1,69 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export const registerForEvent = async (app: FastifyInstance) => {
+  
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post('/events/:eventId/participant', {
+        schema: {
+          body: z.object({
+            name: z.string().min(3).max(50),
+            email: z.string().email(),
+          }),
+          params: z.object({
+            eventId: z.string().uuid()
+          }),
+          response: {
+            201: z.object({
+              participantId: z.number()
+            })
+          }
+        }
+      },
+      async (request, reply) => {
+        const {name, email} = request.body
+        const {eventId} = request.params
+        
+        const [event, amountOfParticipantForEvent] = await Promise.all([
+          prisma.event.findUnique({
+            where: {id: eventId}
+          }),
+          
+          prisma.participant.count({
+            where: {eventId}
+          })
+        ])
+        
+        if (event?.maximumParticipant && amountOfParticipantForEvent >= event?.maximumParticipant) {
+          throw new Error('The maximum number of participants allowed for this event has been reached')
+        }
+        
+        const participantFromEmail = await prisma.participant.findUnique({
+          where: {
+            eventId_email: {
+              email,
+              eventId
+            }
+          }
+        })
+        
+        if (participantFromEmail) {
+          throw new Error('This email is already registered for this event')
+        }
+        
+        const participant = await prisma.participant.create({
+          data: {
+            name,
+            email,
+            eventId
+          }
+        })
+        
+        return reply.status(201).send({participantId: participant.id})
+        
+      }
+    )
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
 
 const app = fastify()
 
@@ -8,5 +9,6 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
+app.register(registerForEvent)
 
 app.listen({port: 3333}).then(() => console.log('HTTP server running at port 3333!'))
