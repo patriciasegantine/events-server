ed26af43ac2cc46dcb531243b6db1a57bbcf6eb2
Patricia Segantine <pnsegantine@gmail.com>
2024-04-07 18:02:51 +0100

    feat: add get participants by Event

---
+++ src/routes/get-event-participants.ts
@@ -0,0 +1,80 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+// todo: add search by: name, email, createdAt
+
+export const getEventParticipants = async (app: FastifyInstance) => {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .get(
+      '/events/:eventId/participants',
+      {
+        schema: {
+          params: z.object({
+            eventId: z.string().uuid()
+          }),
+          
+          querystring: z.object({
+            name: z.string().nullish(),
+            pageNumber: z.string().transform(Number),
+            pageSize: z.string().transform(Number),
+          }),
+          response: {
+            200: z.object({
+              participants: z.array(
+                z.object({
+                  id: z.number(),
+                  name: z.string(),
+                  email: z.string().email(),
+                  createdAt: z.date(),
+                  checkedAt: z.date().nullable(),
+                })
+              )
+            })
+          }
+        }
+      },
+      async (request, reply) => {
+        
+        const {eventId} = request.params
+        const {pageNumber, pageSize} = request.query
+        
+        const participants = await prisma.participant.findMany({
+          select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true,
+            checkIn: {
+              select: {
+                createdAt: true
+              }
+            }
+          },
+          where: {
+            eventId
+          },
+          take: pageSize,
+          skip: (pageNumber - 1) * pageSize,
+          orderBy: {
+            createdAt: 'desc'
+          }
+        })
+        
+        return reply.send({
+          participants: participants.map((participant) => {
+            return {
+              id: participant.id,
+              name: participant.name,
+              email: participant.email,
+              createdAt: participant.createdAt,
+              checkedAt: participant.checkIn?.createdAt ?? null
+            }
+          })
+        })
+      }
+    )
+  
+}
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getParticipantBadge } from "./routes/get-participant-badge";
 import { checkIn } from "./routes/check-in";
+import { getEventParticipants } from "./routes/get-event-participants";
 
 const app = fastify()
 
@@ -16,5 +17,6 @@ app.register(registerForEvent)
 app.register(getEvent)
 app.register(getParticipantBadge)
 app.register(checkIn)
+app.register(getEventParticipants)
 
 app.listen({port: 3333}).then(() => console.log('HTTP server running at port 3333!'))
